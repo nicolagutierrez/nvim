128b146371ee98efc2b4ced06ea12272c771457a
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-04-29 22:49:05 +0200

    fix

--- nvim.bak/init.lua
+++ nvim.bak/init.lua
@@ -1,15 +1,8 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number relativenumber")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
+vim.fn.system({
+  "git",
+  "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
@@ -18,7 +11,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
-
+require("vim-options")
 require("lazy").setup("plugins")
 
